dbe83b7838b7bd4450a6ab1644b5cc2f8e139dcc
sestaton <statonse@gmail.com>
2014-01-02 18:02:26 -0500

    add script for loading xs libs

---
+++ load_xs.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/env perl
+
+## Example of directories used for loading shared libraries
+use lib qw(/iob_home/jmblab/statonse/apps/perlmod/cjfields-Bio-Kseq-dc7a71c/lib/site_perl/5.14.1/x86_64-linux-thread-multi/auto); #/Bio/Kseq/Kseq.bs
+use lib qw(/iob_home/jmblab/statonse/apps/perlmod/cjfields-Bio-Kseq-dc7a71c/lib/site_perl/5.14.1/x86_64-linux-thread-multi); #/Bio/Kseq.pm
+use Bio::Kseq;
