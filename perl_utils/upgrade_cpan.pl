57667e4b8d8c766be24f107024de51e8590d637b
sestaton <statonse@gmail.com>
2014-10-06 10:50:30 -0700

    add method to install deps if they are unavailable

--- perl_utils/upgrade_cpan.pl
+++ perl_utils/upgrade_cpan.pl
@@ -4,12 +4,13 @@
 
 use strict;
 use warnings;
-use File::Find::Rule;
-use JSON;
 
 my $usage = "USAGE: upgrade-cpan.pl 5.14.2 | sort -u | cpanm\n";
 my $old   = shift or die $usage;
- 
+
+load_class("File::Find::Rule");
+load_class("JSON");
+
 my @files = File::Find::Rule->file->name('install.json')->in("$ENV{HOME}/perl5/perlbrew/perls/perl-$old/lib/site_perl/$old");
 for my $file (@files) {
     my $module = parse_json($file)->{name};
@@ -19,4 +20,17 @@ for my $file (@files) {
 sub parse_json {
     open my $fh, shift or die $!;
     JSON::decode_json(join '', <$fh>);
+}
+
+sub load_class {
+    my $class = shift;
+    my $loaded = eval {
+	require $class;
+	$class->import();
+	1;
+    };
+
+    if (!$loaded) {
+	system("cpanm --quiet $class");
+    }
 }
