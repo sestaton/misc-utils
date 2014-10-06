c2b8490929a563275a35bb1ab49f73a3858f6031
sestaton <statonse@gmail.com>
2014-10-06 13:46:51 -0700

    add better (working) method for installing missing deps

--- perl_utils/upgrade_cpan.pl
+++ perl_utils/upgrade_cpan.pl
@@ -4,12 +4,12 @@
 
 use strict;
 use warnings;
+use JSON;
 
 my $usage = "USAGE: upgrade-cpan.pl 5.14.2 | sort -u | cpanm\n";
 my $old   = shift or die $usage;
 
-load_class("File::Find::Rule");
-load_class("JSON");
+load_classes("File::Find::Rule", "JSON");
 
 my @files = File::Find::Rule->file->name('install.json')->in("$ENV{HOME}/perl5/perlbrew/perls/perl-$old/lib/site_perl/$old");
 for my $file (@files) {
@@ -22,15 +22,20 @@ sub parse_json {
     JSON::decode_json(join '', <$fh>);
 }
 
-sub load_class {
-    my $class = shift;
-    my $loaded = eval {
-	require $class;
-	$class->import();
-	1;
-    };
+sub load_classes {
+    my @classes = @_;
+    
+    for my $class (@classes) {
+	my $loaded = eval {
+	    eval "require $class";
+	    $class->import();
+	    1;
+	};
 
-    if (!$loaded) {
-	system("cpanm --quiet $class");
+	unless ($loaded) {
+	    system("cpanm $class");
+	    eval "require $class";
+	    $class->import->();
+	}
     }
 }
