c7a8e67fc0887195468db23f0b2ea69d41aa933a
sestaton <statonse@gmail.com>
2014-11-07 15:15:38 -0500

    add digital ocean client

---
+++ perl_utils/do_client.pl
@@ -0,0 +1,107 @@
+#!/usr/bin/env perl
+
+use 5.010;
+use strict;
+use warnings;
+use Config::Tiny;
+use DigitalOcean;
+use Data::Dump;
+
+my $config = Config::Tiny->read( '/home/jmblab/statonse/.digitalocean', 'utf8' );
+my $do_obj = DigitalOcean->new(%{ $config->{doauth} });
+ 
+my $size_id   = get_sizes($do_obj);
+my $image_id  = get_images($do_obj);
+my $region_id = get_regions($do_obj); 
+my $ssh_id    = get_keys($do_obj);
+
+#my $t0 = time;
+#my $droplet = $do_obj->create_droplet(
+#    name          => 'demo1',
+#    size_id       => $size_id,
+#    image_id      => $image_id,
+#    region_id     => $region_id,
+#    ssh_key_ids   => $ssh_id,
+#    wait_on_event => 1,
+#);
+#my $t1 = time;
+
+#my $password = $droplet->password_reset;
+#dd $password;
+#my $server = $do_obj->droplet($droplet->id);
+
+#say "password ", $password;
+#say "Created droplet: ",$server->id, " with name: ", $server->name, 
+#    " and IP: ", $server->ip_address, " in ",$t1-$t0, " seconds.";
+
+#say "Running droplets:";
+get_droplets($do_obj);
+
+#my $cmd = sprintf 'ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@%s "uname -a; uptime; hostname"',
+#    $server->ip_address;
+#say $cmd;
+#system $cmd;
+ 
+#$server->destroy;
+#
+# methods
+#
+sub get_sizes {
+    my ($do_obj) = @_;
+
+    my $size_id;
+    my $sizes = $do_obj->sizes;
+    for my $s (@$sizes) {
+	#printf "%6s  %s  %5s\n", $s->name, $s->id, $s->slug;
+	if ($s->name =~ /512MB/i && $s->slug =~ /512MB/i) {
+	    $size_id = $s->id;
+	}
+    }
+    return $size_id;
+}
+
+sub get_regions {
+    my ($do_obj) = @_;
+
+    my $region_id;
+    for my $r (@{ $do_obj->regions }) {
+	#printf "%2s  %-15s - %s\n", $r->id, $r->name, $r->slug;
+	if ($r->name =~ /San Francisco/i) {
+	    $region_id = $r->id;
+	}
+    }
+    return $region_id;
+}
+
+sub get_images {
+    my ($do_obj) = @_;
+    
+    my $image_id;
+    my $images = $do_obj->images;
+    for my $img (@$images) {
+	#printf "%-10s %7s  %-50s - %s\n", $img->distribution, $img->id, $img->name, ($img->slug || '');
+	if ($img->distribution =~ /ubuntu/i && $img->name =~ /14\.10\s+x64/ix) {
+	    $image_id = $img->id;
+	}
+    }
+    return $image_id;
+}
+
+sub get_keys {
+    my ($do_obj) = @_;
+
+    my $ssh_id;
+    for my $ssh (@{ $do_obj->ssh_keys }) {
+	#printf "%s   %s \n", $ssh->id, $ssh->name;
+	$ssh_id = $ssh->id;
+    }
+    return $ssh_id;
+}
+
+sub get_droplets {
+    my ($do_obj) = @_;
+
+    for my $droplet (@{$do_obj->droplets}) {
+	printf "Droplet %s has id %s and IP address %s\n", $droplet->name, $droplet->id, $droplet->ip_address;
+    }
+}
