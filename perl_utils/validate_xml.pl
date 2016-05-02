18afe38ee1b74c8e011f19910a7ea35e9852f7be
Evan Staton <statonse@gmail.com>
2016-05-01 19:40:03 -0700

    take file as argument

--- perl_utils/validate_xml.pl
+++ perl_utils/validate_xml.pl
@@ -3,12 +3,16 @@
 use 5.010;
 use strict;
 use warnings;
+use File::Basename;
 use XML::LibXML;
+
+my $usage = basename($0).' file.xml';
+my $xml   = shift or die $usage;
 
 my $parser = XML::LibXML->new();
 $parser->validation(1);
 
-eval { say "xml is valid=".$parser->parse_file('ch2.xml')->is_valid; };
+eval { say "xml is valid=".$parser->parse_file($xml)->is_valid; };
 
 if ($@) { say "dtd not valid: $@"; }
 else    { say "dtd valid"; }
